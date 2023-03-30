52ce18d85d300d10982c281774818ce42cf11493
Prachi Meshram <prachimesh89@gmail.com>
2023-03-30 11:45:50 +0530

    Wave Array  DONE

---
+++ InterviewBit/waveArray.cpp
@@ -0,0 +1,14 @@
+// https://www.interviewbit.com/problems/wave-array/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> wave(vector<int> &A) {
+    sort(A.begin(), A.end());
+    
+    for(int i = 1; i<A.size(); i += 2) {
+        swap(A[i], A[i-1]);
+    }
+    
+    return A;
+}
