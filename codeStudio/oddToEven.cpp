cbb0d8f050294fb28e9386414052f6eea2d21a4d
Prachi Meshram <prachimesh89@gmail.com>
2023-04-11 09:39:32 +0530

    odd to even DONE

---
+++ codeStudio/oddToEven.cpp
@@ -0,0 +1,33 @@
+// https://www.codingninjas.com/codestudio/problem-details/odd-to-even_1214553
+
+#include <bits/stdc++.h> 
+string oddToEven(string &num) 
+{
+	// Write your code here
+	string nums = num;
+	int n = num.size();
+
+	int temp = -1;
+
+	int i = 0;
+	int j= n-1;
+
+	while (i<=n-2) {
+		if (!(num[i] & 1)) {
+			temp = i;
+			if (num[i] < num[j]) {
+				swap(num[i], num[j]);
+				return num;
+			}	
+		}
+		i++;
+	}
+	if (temp != -1) {
+		swap(num[temp], num[j]);
+		return num;
+	}
+	
+
+	return "-1";
+}
+
