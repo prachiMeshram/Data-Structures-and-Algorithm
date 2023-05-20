72fc8d39333e4b92a4dbc7537ec83f24328402d0
Prachi Meshram <prachimesh89@gmail.com>
2023-05-20 12:12:04 +0530

    added practice file and bit manipulation folder

---
+++ Practice/BitManipulation/ithBit.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getIthBit(int n, int i) {
+    int bitMask = 1 << 1;
+
+    if (n & bitMask == 0) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+int main () {
+    int n;
+    cout << "enter number: "<< endl;
+    cin>> n;
+
+    int i;
+    cout << "enter position: " << endl;
+    cin>> i;
+    
+    cout << i << "th Bit is: "  << getIthBit(n, i);
+
+    return 0;
+}
