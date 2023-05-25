fdae652666f4af58ad512753c0e29faa99d36589
Prachi Meshram <prachimesh89@gmail.com>
2023-05-25 09:03:36 +0530

    nth fibonacci done

--- Practice/Reccursion/nthFibonacciNum.cpp
+++ Practice/Reccursion/nthFibonacciNum.cpp
@@ -1,7 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main () {
+int nthFibNum (int n) {
+    // base case 
+    if (n == 1 || n == 0) {
+        return n;
+    }
 
+    return nthFibNum(n-1) + nthFibNum(n-2);
+}
+
+int main () {
+    int n;
+    cin >> n;
+
+    cout << nthFibNum(n) << " ";
     return 0;
 }
