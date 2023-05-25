e99c357a67e6e60272fe2258b0a28e5562ab901c
Prachi Meshram <prachimesh89@gmail.com>
2023-05-25 09:37:57 +0530

    first occurence of elem done

--- Practice/Reccursion/firstOccurOfElem.cpp
+++ Practice/Reccursion/firstOccurOfElem.cpp
@@ -1,7 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main () {
+int firstOccu(int index, int arr[], int key, int size) {
+    // base case 
+    if (index == size-1) {
+        return -1;
+    }
 
+    if (arr[index] == key) {
+        return index;
+    }
+
+    firstOccu (index + 1, arr, key, size);
+}
+
+int main () {
+    int arr[] = {1, 2, 3, 4, 5, 11, 7, 8, 11};
+    int k;
+    cin >> k;
+    int size = 9;
+    cout << firstOccu(0, arr, k, size) << " ";
     return 0;
 }
