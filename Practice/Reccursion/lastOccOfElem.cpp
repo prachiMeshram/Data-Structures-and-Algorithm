e6204b0701a3d63e2c2c7f92e0748671ed4665cd
Prachi Meshram <prachimesh89@gmail.com>
2023-05-25 10:07:56 +0530

    Last occurence of elem with alpha approach done

--- Practice/Reccursion/lastOccOfElem.cpp
+++ Practice/Reccursion/lastOccOfElem.cpp
@@ -1,7 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int firstOccu(int index, int arr[], int key) {
+// alpha approach
+int lastOccurence (int index, int arr[], int key, int n) {
+    
+
+    // base case 
+    if (index == n) {
+        return -1;
+    }
+
+    int isFound = lastOccurence(index+1, arr, key, n);
+
+    if (isFound == -1 && arr[index] == key) {
+        return index;
+    }
+
+    return isFound;
+}
+
+
+
+// my aproach
+int lastOccu(int index, int arr[], int key) {
     // base case 
     if (index < 0) {
         return -1;
@@ -11,14 +32,14 @@ int firstOccu(int index, int arr[], int key) {
         return index;
     }
 
-    firstOccu (index - 1, arr, key);
+    lastOccu (index - 1, arr, key);
 }
 
 int main () {
     int arr[] = {1, 2, 11, 4, 5, 11, 7, 8, 10};
     int k;
     cin >> k;
-    int size = 9;
-    cout << firstOccu(size-1, arr, k) << " ";
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << lastOccurence(0, arr, k, n) << " ";
     return 0;
 }
