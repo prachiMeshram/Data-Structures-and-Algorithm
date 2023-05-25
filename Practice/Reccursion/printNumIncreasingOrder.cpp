37ef7c18ab9045d791b9f001077502e9885529b6
Prachi Meshram <prachimesh89@gmail.com>
2023-05-25 08:51:40 +0530

    print numbers in increasing order done

--- Practice/Reccursion/printNumIncreasingOrder.cpp
+++ Practice/Reccursion/printNumIncreasingOrder.cpp
@@ -1,7 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main () {
+void printNumsIncreOrder (int num) {
+    // base case 
+    if (num == 0) {
+        return;
+    }
 
+    printNumsIncreOrder (num-1);
+
+    cout << num << " ";
+}
+
+int main () {
+    int num;
+
+    cin >> num;
+
+    printNumsIncreOrder (num);
     return 0;
 }
