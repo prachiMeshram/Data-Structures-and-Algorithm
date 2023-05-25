7479c8e4ae9dfa10478ff96f26536d4400e4bf43
Prachi Meshram <prachimesh89@gmail.com>
2023-05-25 09:40:31 +0530

    Last occurence of elem done

--- Practice/Reccursion/lastOccOfElem.cpp
+++ Practice/Reccursion/lastOccOfElem.cpp
@@ -1,7 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main () {
+int firstOccu(int index, int arr[], int key) {
+    // base case 
+    if (index < 0) {
+        return -1;
+    }
 
+    if (arr[index] == key) {
+        return index;
+    }
+
+    firstOccu (index - 1, arr, key);
+}
+
+int main () {
+    int arr[] = {1, 2, 11, 4, 5, 11, 7, 8, 10};
+    int k;
+    cin >> k;
+    int size = 9;
+    cout << firstOccu(size-1, arr, k) << " ";
     return 0;
 }
