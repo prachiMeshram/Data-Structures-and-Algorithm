292df16f7a2ca1f9fa50e3459dd6fca1371c5b49
Prachi Meshram <prachimesh89@gmail.com>
2023-05-25 10:28:45 +0530

    power(a,b) optimised approach done

--- Practice/Reccursion/power(a,b).cpp
+++ Practice/Reccursion/power(a,b).cpp
@@ -1,7 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main () {
+int power2(int a, int b) {
+    // base case 
+    if (b == 0) {
+        return 1;
+    }
 
+    int halfPower = power2(a, b/2);
+    // int halfPowSq = power2(a, b/2) * power2(a, b/2); // this will increase TC
+    int halfPowSq = halfPower*halfPower;
+
+    if (b & 1 == 1) {
+        halfPowSq = a * halfPowSq;
+    }
+
+    return halfPowSq;
+}
+
+int power1(int a, int b) {
+    // base case 
+    if (b == 0) {
+        return 1;
+    }
+    
+    return a * power1(a,b-1);
+}
+
+int main () {
+    int a, b;
+    cin >> a >> b;
+
+    cout << power2(a,b) << " ";
     return 0;
 }
