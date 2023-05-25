205778b50d6955a03e34005cadd4b4f5dbf547b5
Prachi Meshram <prachimesh89@gmail.com>
2023-05-25 10:42:48 +0530

    TILING problem done

--- Practice/Reccursion/tiling.cpp
+++ Practice/Reccursion/tiling.cpp
@@ -1,7 +1,30 @@
+// TILING PROBLEM 
+
+// Given a "2*n" board and tiles of size "2*1", count the number of ways to tile the given board using the 2*1 tiles.
+// (A tile can either be placed horizontally or vertically)
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int main () {
+int solveTiling (int n) {
+    // Base Case
+    if (n == 0 || n == 1) {
+        return 1;
+    }
 
+    // vertically 
+    int fnm1 = solveTiling(n-1);
+    // horizontally 
+    int fnm2 = solveTiling(n-2);
+
+    int totalWays = fnm1 + fnm2;
+
+    return totalWays;
+}
+
+int main () {
+    int num;
+    cin >> num;
+    cout << solveTiling(num)<< " ";
     return 0;
 }
