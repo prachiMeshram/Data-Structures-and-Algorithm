b3cb6e2a7c4c8ce894b95015d4bce5953f2bffcc
Prachi Meshram <prachimesh89@gmail.com>
2023-05-25 09:16:27 +0530

    Check Array Is Sorted  done

--- Practice/Reccursion/checkArrayIsSortedOrNot.cpp
+++ Practice/Reccursion/checkArrayIsSortedOrNot.cpp
@@ -1,7 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main () {
+bool checkSortArray(int index, int arr[], int size) {
+    // base case 
+    if (index == size-1) {
+        return true;
+    }
 
+    if (arr[index] > arr[index+1]) {
+        return false;
+    }
+    return checkSortArray(index + 1, arr, size);
+}
+
+int main () {
+    int arr[] = {1, 2, 3, 4, 5, 11, 7, 8, 9};
+
+    cout << checkSortArray(0, arr, 9) << " ";
     return 0;
 }
