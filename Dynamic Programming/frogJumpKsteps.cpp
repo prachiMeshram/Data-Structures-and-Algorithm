11a95544d7f60318de9559859b075d365fa97637
Prachi Meshram <prachimesh89@gmail.com>
2023-05-29 01:02:31 +0530

    Frog jumps in k steps (tabulation) DONE

--- Dynamic Programming/frogJumpKsteps.cpp
+++ Dynamic Programming/frogJumpKsteps.cpp
@@ -1,24 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int frogKjumps(int i, vector<int> &heights, vector<int> &dp, int k) {
-    // base case 
-    if (i == 0) {
-        return 0;
-    }
-    if (dp[i] != -1) {
-        return dp[i];
-    }
+// int frogKjumps(int i, vector<int> &heights, vector<int> &dp, int k) {
+//     // base case 
+//     if (i == 0) {
+//         return 0;
+//     }
+//     if (dp[i] != -1) {
+//         return dp[i];
+//     }
 
-    int minSteps = INT_MAX;
+//     int minSteps = INT_MAX;
 
-    for (int j = 1; j <= k; j++) {
-        if (i - j >= 0) {
-            int jumps = frogKjumps(i - j, heights, dp, k) + abs(heights[i] - heights[i - j]);
-            minSteps = min(minSteps, jumps);
+//     for (int j = 1; j <= k; j++) {
+//         if (i - j >= 0) {
+//             int jumps = frogKjumps(i - j, heights, dp, k) + abs(heights[i] - heights[i - j]);
+//             minSteps = min(minSteps, jumps);
+//         }
+//     }
+//     return dp[i] = minSteps;
+// }
+
+// tabulation approach
+int frogKjumpsKsteps(int n, vector<int> &heights, vector<int> &dp, int k) {
+    dp[0] = 0;
+    
+    for (int i = 1; i < n; i++) {
+
+        int minSteps = INT_MAX;
+        for (int j = 1; j <= k; j++) {
+            if (i-j >= 0) {
+                int jumps = dp[i-j] + abs(heights[i] - heights[i - j]);
+                minSteps = min(jumps, minSteps);
+            }
         }
+        dp[i] = minSteps;
     }
-    return dp[i] = minSteps;
+
+    return  dp[n-1];
 }
 
 int main() {
@@ -26,7 +45,8 @@ int main() {
     int n = heights.size();
     int k = 2;
     vector<int> dp(n, -1);
-    cout << frogKjumps(n - 1, heights, dp, k);
+    // cout << frogKjumps(n - 1, heights, dp, k);
+    cout << frogKjumpsKsteps(n, heights, dp, k);
 
     return 0;
 }
