52aecfcdf34b551bc6aaf2f6a59ce3cd2943128a
Prachi Meshram <prachimesh89@gmail.com>
2023-05-29 12:01:03 +0530

    maximum stored water (Two pointer approach)

--- Practice/Vectors/containerWithMostWater.cpp
+++ Practice/Vectors/containerWithMostWater.cpp
@@ -2,15 +2,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int maxWater(vector<int> &vec) {
-    int maxi = INT_MIN;
+// BRUTE FORCE
+// int maxWater(vector<int> &vec) {
+//     int maxi = INT_MIN;
 
-    for (int i = 0; i < vec.size(); i++) {
-        for (int j = i+1; j < vec.size(); j++) {
-            int height = min(vec[i], vec[j]);
-            int width = j-i;
-            int water = height*width;
-            maxi = max(maxi, water);
+//     for (int i = 0; i < vec.size(); i++) {
+//         for (int j = i+1; j < vec.size(); j++) {
+//             int height = min(vec[i], vec[j]);
+//             int width = j-i;
+//             int water = height*width;
+//             maxi = max(maxi, water);
+//         }
+//     }
+
+//     return maxi;
+// }
+
+// TWO POINTER APPROACH
+int maxWater(vector<int> &vec) {
+
+    int maxi = 0;
+    int l = 0;
+    int r = vec.size()-1;
+
+    while (l<r) {
+        int height = min(vec[l], vec[r]);
+        int width = r-l;
+        int water = height*width;
+        maxi = max(maxi, water);
+        if (vec[l] < vec[r]) {
+            l++;
+        }
+        else {
+            r--;
         }
     }
 
