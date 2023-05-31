49bb2dcb3bc659a5eebf3386cd005c6a28e3fb26
Prachi Meshram <prachimesh89@gmail.com>
2023-06-01 02:01:04 +0530

    Ninja’s Training TABULATION

--- Dynamic Programming/ninjaTraining.cpp
+++ Dynamic Programming/ninjaTraining.cpp
@@ -38,3 +38,32 @@ int ninjaTraining(int n, vector<vector<int>> &points)
     return maxPoints(n-1, 3, dp, points);
 
 }
+
+int ninjaTrainingTabulation(int n, vector<vector<int>> &points)
+{
+    // Write your code here.
+    vector<vector<int>> dp(n, vector<int>(4, 0));
+    
+    // return maxPoints(n-1, 3, dp, points);
+
+    dp[0][0] = max(points[0][1], points[0][2]);
+    dp[0][1] = max(points[0][0], points[0][2]);
+    dp[0][2] = max(points[0][1], points[0][0]);
+    dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]) );
+
+    for (int day=1; day<n; day++) {
+        for (int last = 0; last < 4; last++) {
+            // int maxi = 0;
+            for (int task=0; task<3; task++) {
+                if (task != last) {
+                    int pts = points[day][task] + dp[day-1][task];
+                    // maxi = max(maxi, pts);
+                    dp[day][last] = max(dp[day][last], pts);
+                }
+            }
+            // dp[day][last] = maxi; 
+        }
+    }
+
+    return dp[n-1][3];
+}
