b006c735855f3c0c90e8114d81b19ae2d74a363c
Prachi Meshram <prachimesh89@gmail.com>
2023-06-02 11:26:19 +0530

    MergeSort Linked List

---
+++ Practice/LinkedList/mergeSortLinkedList.cpp
@@ -0,0 +1,98 @@
+// https://www.codingninjas.com/codestudio/problems/mergesort-linked-list_630514?leftPanelTab=0&campaign=YouTube_CodestudioLovebabbar5thfeb&utm_source=youtube&utm_medium=affiliate&utm_campaign=YouTube_CodestudioLovebabbar5thfeb
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Following is the representation of the Singly Linked List Node:
+
+    class node{
+        public:
+            int data;
+            node * next;
+            node(int data){
+                this->data=data;
+                this->next=NULL;
+            }
+    };
+    
+
+
+node* getMid(node* head) {
+
+    node* slow = head;
+    node* fast = head->next;
+
+    while (fast != NULL && fast -> next != NULL) {
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    }
+
+    return  slow;
+}
+
+node* merge(node* left, node* right) {
+
+    if (left == NULL) {
+        return right;
+    }
+    
+    if (right == NULL) {
+        return left;
+    }
+
+    node* ans =new node(-1);
+    node* temp = ans;
+
+    while (left != NULL && right != NULL) {
+        if (left -> data  <  right -> data) {
+            temp -> next = left;
+            temp = left;
+            left = left -> next;
+        }
+        else {
+            temp -> next = right;
+            temp = right;
+            right = right -> next;
+        }
+    }
+
+    while (left != NULL) {
+        temp -> next = left;
+        temp = left;
+        left = left -> next;
+        // temp -> next = left;
+        // left = left -> next;
+        // temp = temp -> next;
+    }
+
+    while (right != NULL) {
+        temp -> next = right;
+        temp = right;
+        right = right -> next;
+        // temp -> next = right;
+        // right = right -> next;
+        // temp = temp -> next;
+    }
+    ans = ans-> next;
+    return ans;
+}
+
+node* mergeSort(node *head) {
+    // Write your code here.
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    node* mid = getMid(head);
+
+    node* left = head;
+    node* right = mid -> next;
+    mid -> next = NULL;
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    node* res = merge(left, right);
+
+    return res;
+}
