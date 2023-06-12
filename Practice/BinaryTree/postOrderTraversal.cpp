fcc5086191a17427b072c70e2ff86563a025032a
Prachi Meshram <prachimesh89@gmail.com>
2023-06-12 17:53:29 +0530

    Binary Tree postorder Traversal ITERATIVELY using SINGLE STACK

--- Practice/BinaryTree/postOrderTraversal.cpp
+++ Practice/BinaryTree/postOrderTraversal.cpp
@@ -59,6 +59,40 @@ void solve(TreeNode* root, vector<int> &ans) {
     }
 }
 
+// ITERATION SINGLE STACKS
+void solve(TreeNode* root, vector<int> &ans) {
+    if (root == NULL) {
+        return;
+    }
+    stack<TreeNode*> st;
+    TreeNode* curr = root;
+
+    while (curr || !st.empty()) {
+
+        if (curr) {
+            st.push(curr);
+            curr = curr->left;
+        }
+        else {
+            TreeNode* temp = st.top()->right;
+            if (temp == NULL) {
+                temp = st.top();
+                st.pop();
+                ans.push_back(temp->val);
+
+                while (!st.empty() && temp == st.top()->right) {
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                }
+            }
+            else {
+                curr = temp;
+            }
+        }
+    }
+}
+
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
