ea4bd40c49f2c7bb44021812f634117e4a476047
Prachi Meshram <prachimesh89@gmail.com>
2023-06-12 23:35:37 +0530

    Sum Tree

---
+++ Practice/BinaryTree/sumTree.cpp
@@ -0,0 +1,51 @@
+// https://practice.geeksforgeeks.org/problems/sum-tree/1
+
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left, * right;
+};
+
+// Should return true if tree is Sum Tree, else false
+class Solution
+{
+    private:
+    pair<bool, int> isSumTreeFast(Node* root) {
+        if (root == NULL) {
+            pair<bool, int> p = make_pair(true, 0);
+            return p;
+        }
+        if (root -> left == NULL && root->right == NULL) {
+            pair<bool, int> p = make_pair(true, root->data);
+            return p;
+        }
+        
+        pair<bool, int> leftAns = isSumTreeFast(root->left);
+        pair<bool, int> rightAns = isSumTreeFast(root->right);
+        
+        bool left = leftAns.first;
+        bool right = rightAns.first;
+        
+        bool sum = root->data == leftAns.second + rightAns.second;
+        
+        pair<bool, int> ans;
+        if (left && right && sum) {
+            ans.first = true;
+            ans.second = 2*root->data;
+        }
+        else {
+            ans.first = false;
+        }
+        
+        return ans;
+    }
+    public:
+    bool isSumTree(Node* root)
+    {
+         // Your code here
+         return isSumTreeFast(root).first;
+    }
+};
