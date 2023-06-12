cc6514be232ece6ee48e023b3735bb38c0ca9622
Prachi Meshram <prachimesh89@gmail.com>
2023-06-13 00:29:33 +0530

    Unique Paths II RECURSIVELY with DP

--- Dynamic Programming/uniquePathsII.cpp
+++ Dynamic Programming/uniquePathsII.cpp
@@ -27,3 +27,35 @@ int mazeObstacles(int n, int m, vector< vector< int> > &mat) {
     // Write your code here
     return solve(n-1, m-1, mat);
 }
+
+// NORMAL RECURSIVELY with DP
+int mod = (int) (1e9 + 7);
+int solve(int i, int j, vector< vector< int> > &mat, vector<vector<int>> &dp) {
+    if (i == 0 && j == 0) {
+        dp[0][0] = 1;
+        return 1;
+    }
+    if (i>=0 && j>= 0 && mat[i][j] == -1) {
+        dp[i][j] = 0;
+        return 0;
+    }
+    if (i < 0 || j < 0) {
+        return 0;
+    }
+    if (dp[i][j] != -1) {
+        return dp[i][j];
+    }
+
+    int up = solve(i-1, j, mat, dp);
+    int left = solve(i, j-1, mat, dp);
+    dp[i][j] = (up + left)%mod;
+
+    return dp[i][j];
+}
+
+int mazeObstacles(int n, int m, vector< vector< int> > &mat) {
+    // Write your code here
+    vector<vector<int>> dp (n, vector<int> (m, -1));
+    solve(n-1, m-1, mat, dp);
+    return dp[n-1][m-1];
+}
