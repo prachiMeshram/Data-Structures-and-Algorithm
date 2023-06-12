30721ce43d23b93ca685c4144d5b838a5605ef76
Prachi Meshram <prachimesh89@gmail.com>
2023-06-12 23:03:35 +0530

    Check for Balanced Tree brute force OPTIMISED

--- Practice/BinaryTree/checkforBalancedTree.cpp
+++ Practice/BinaryTree/checkforBalancedTree.cpp
@@ -45,3 +45,37 @@ class Solution{
         return false;
     }
 };
+// OPTIMISED SPACE & TIME COMPLEXITY 
+class Solution{
+    public:
+    //Function to check whether a binary tree is balanced or not.
+    pair<bool, int> isBalancedFast(Node* root) {
+        if (root == NULL) {
+            pair<bool, int> p = make_pair(1, 0);
+            return p;
+        }
+        
+        pair<bool, int> left = isBalancedFast(root->left);
+        pair<bool, int> right = isBalancedFast(root->right);
+        bool diff = abs(left.second - right.second) <= 1;
+        
+        pair<bool, int> ans;
+        ans.second = max(left.second, right.second) + 1;
+        
+        if (left.first && right.first && diff) {
+            ans.first = true;
+        }
+        else {
+            ans.second = false;
+        }
+        
+        return ans;
+    }
+    bool isBalanced(Node *root)
+    {
+        //  Your Code here
+        pair<bool, int> res = isBalancedFast(root);
+        
+        return res.first;
+    }
+};
