1f0fec2e4589b080b1e2e8fbadb32438d3f1f2a5
Prachi Meshram <prachimesh89@gmail.com>
2023-06-12 22:45:09 +0530

    Check for Balanced Tree brute force

---
+++ Practice/BinaryTree/checkforBalancedTree.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+class Solution{
+    private:
+    int height(Node *root) {
+        if (root == NULL) {
+            return 0;
+        }
+        
+        int left = height(root->left);
+        int right = height(root->right);
+        
+        return max(left, right) + 1;
+    }
+    public:
+    //Function to check whether a binary tree is balanced or not.
+    bool isBalanced(Node *root)
+    {
+        //  Your Code here
+        if (root == NULL) {
+            return true;
+        }
+        
+        bool left = isBalanced(root->left);
+        bool right = isBalanced(root->right);
+        
+        bool diff = (abs(height(root->left)-height(root->right))) <=1;
+        
+        if (left && right && diff) {
+            return true;
+        }
+        return false;
+    }
+};
