3540b2b98ea761b2ffe3449b92433e23d45e352c
Prachi Meshram <prachimesh89@gmail.com>
2023-06-13 11:44:14 +0530

    Minimum Path Sum TABULATION

--- Dynamic Programming/minimumPathSum.cpp
+++ Dynamic Programming/minimumPathSum.cpp
@@ -54,3 +54,39 @@ int minSumPath(vector<vector<int>> &grid) {
 
     return solve(n-1, m-1, grid, dp);
 }
+
+// TABULATION
+int minSumPath(vector<vector<int>> &grid) {
+  // Write your code here.
+
+  int n = grid.size();
+  int m = grid[0].size();
+  vector<vector<int>> dp(n, vector<int>(m, -1));
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (i == 0 && j == 0) {
+        dp[i][j] = grid[i][j];
+      } 
+      else {
+          int up = 0; 
+          int left = 0; 
+          if (i > 0) {
+            up = grid[i][j] + dp[i - 1][j];
+          }
+          else {
+              up = INT_MAX;
+          }
+          if (j > 0) {
+            left = grid[i][j] + dp[i][j - 1];
+          }
+          else {
+              left = INT_MAX;
+          }
+          dp[i][j] = min(up, left);
+      }
+    }
+  }
+
+  return dp[n-1][m-1];
+}
