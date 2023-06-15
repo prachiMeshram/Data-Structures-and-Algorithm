0b5191ac2a9c1ff36ee126c6269efa3aae2e48f1
Prachi Meshram <prachimesh89@gmail.com>
2023-06-15 12:23:41 +0530

    chocolate pickup RECURSION

---
+++ Dynamic Programming/chocolatePickup.cpp
@@ -0,0 +1,41 @@
+// https://www.codingninjas.com/codestudio/problems/ninja-and-his-friends_3125885?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// normal recursion
+int f(int i, int j1, int j2, vector<vector<int>> &grid, int r, int c) {
+  // out of bound cases
+  if (j1 < 0 || j2 < 0 || j1 >= c || j2 >= c) {
+    return -1e8;
+  }
+  // base case
+  if (i == r - 1) {
+    if (j1 == j2) {
+      return grid[i][j1];
+    } else {
+      return grid[i][j1] + grid[i][j2];
+    }
+  }
+  int maxi = -1e8;
+  for (int dj1 = -1; dj1 <= 1; dj1++) {
+    for (int dj2 = -1; dj2 <= 1; dj2++) {
+      int ans = 0;
+      if (j1 == j2) {
+        ans = grid[i][j1];
+      } else {
+        ans = grid[i][j1] + grid[i][j2];
+      }
+      ans += f(i + 1, j1 + dj1, j2 + dj2, grid, r, c);
+      maxi = max(maxi, ans);
+    }
+  }
+
+  return maxi;
+}
+
+int maximumChocolates(int r, int c, vector<vector<int>> &grid) {
+  // Write your code here.
+
+  return f(0, 0, c - 1, grid, r, c);
+}
