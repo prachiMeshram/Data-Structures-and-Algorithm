cd621d4dcee6f550ec8dd81553d283e6684c836d
Prachi Meshram <prachimesh89@gmail.com>
2023-06-15 16:04:23 +0530

    chocolate pickup  Tabulation

--- Dynamic Programming/chocolatePickup.cpp
+++ Dynamic Programming/chocolatePickup.cpp
@@ -86,3 +86,50 @@ int maximumChocolates(int r, int c, vector<vector<int>> &grid) {
 
   return f(0, 0, c - 1, grid, r, c, dp);
 }
+
+// TABULATIOn
+int maximumChocolates(int r, int c, vector<vector<int>> &grid) {
+  // Write your code here.
+  vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c, -1)));
+
+  // base case r-1
+  for (int j1 = 0; j1 < c; j1++) {
+    for (int j2 = 0; j2 < c; j2++) {
+      if (j1 == j2) {
+        dp[r - 1][j1][j2] = grid[r - 1][j1];
+      } else {
+        dp[r - 1][j1][j2] = grid[r - 1][j1] + grid[r - 1][j2];
+      }
+    }
+  }
+
+  for (int i = r - 2; i >= 0; i--) {
+    for (int j1 = 0; j1 < c; j1++) {
+      for (int j2 = 0; j2 < c; j2++) {
+
+        int maxi = INT_MIN;
+        for (int dj1 = -1; dj1 <= 1; dj1++) {
+          for (int dj2 = -1; dj2 <= 1; dj2++) {
+            int ans;
+            if (j1 == j2) {
+              ans = grid[i][j1];
+            } else {
+              ans = grid[i][j1] + grid[i][j2];
+            }
+            if (j1+dj1 >=0 && j2+dj2 >=0 && j1+dj1 < c && j2+dj2 <c) {
+              ans += dp[i+1][j1+dj1][j2+dj2];
+            }
+            else {
+              ans += -1e9;
+            }
+            
+            maxi = max(maxi, ans);
+          }
+        }
+        dp[i][j1][j2] = maxi;
+      }
+    }
+  }
+  return dp[0][0][c-1];
+}
+
