2ee213c6fb48ff7b6918bdcc85a5cd83dc7a8dc1
Prachi Meshram <prachimesh89@gmail.com>
2023-06-15 12:56:20 +0530

    chocolate pickup  recursion + DP

--- Dynamic Programming/chocolatePickup.cpp
+++ Dynamic Programming/chocolatePickup.cpp
@@ -39,3 +39,50 @@ int maximumChocolates(int r, int c, vector<vector<int>> &grid) {
 
   return f(0, 0, c - 1, grid, r, c);
 }
+
+
+// recursion + DP 
+int f(int i, int j1, int j2, vector<vector<int>> &grid, int r, int c,
+      vector<vector<vector<int>>> &dp) {
+  // out of bound cases
+  
+  if (j1 < 0 || j2 < 0 || j1 >= c || j2 >= c) {
+    return -1e9;
+  }
+  
+  // base case
+  if (i == r - 1) {
+    if (j1 == j2) {
+      return grid[i][j1];
+    } else {
+      return grid[i][j1] + grid[i][j2];
+    }
+  }
+
+  if (dp[i][j1][j2] != -1) {
+    return dp[i][j1][j2];
+  }
+
+  int maxi = INT_MIN;
+  for (int dj1 = -1; dj1 <= 1; dj1++) {
+    for (int dj2 = -1; dj2 <= 1; dj2++) {
+      int ans = 0;
+      if (j1 == j2) {
+        ans = grid[i][j1];
+      } else {
+        ans = grid[i][j1] + grid[i][j2];
+      }
+      ans += f(i + 1, j1 + dj1, j2 + dj2, grid, r, c, dp);
+      maxi = max(maxi, ans);
+    }
+  }
+
+  return dp[i][j1][j2] = maxi;
+}
+
+int maximumChocolates(int r, int c, vector<vector<int>> &grid) {
+  // Write your code here.
+  vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c, -1)));
+
+  return f(0, 0, c - 1, grid, r, c, dp);
+}
