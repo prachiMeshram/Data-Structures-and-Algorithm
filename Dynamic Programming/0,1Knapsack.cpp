51592582fed3138aecc4c3501910e525930cc93f
Prachi Meshram <prachimesh89@gmail.com>
2023-06-21 13:01:39 +0530

    0 1 Knapsack recursion TABULATION

--- Dynamic Programming/0,1Knapsack.cpp
+++ Dynamic Programming/0,1Knapsack.cpp
@@ -32,3 +32,31 @@ int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
 	vector<vector<int>> dp(n, vector<int> (maxWeight+1, -1));
 	return fun(n-1, weight, value, maxWeight, dp);
 }
+
+
+// TABULATION
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+	// Write your code here
+	vector<vector<int>> dp(n, vector<int> (maxWeight+1, 0));
+
+	for (int w = weight[0]; w <= maxWeight; w++) {
+		dp[0][w] = value[0];
+	}
+
+	for (int ind = 1; ind < n; ind++) {
+		for (int w = 0; w <= maxWeight; w++) {
+
+			int notTake = 0 + dp[ind-1][w];
+
+			int take = INT_MIN;
+			if (weight[ind] <= w) {
+				take = value[ind] + dp[ind-1][w-weight[ind]];
+			}
+
+			dp[ind][w] = max(take, notTake);
+		}
+	}
+
+	return dp[n-1][maxWeight];
+}
