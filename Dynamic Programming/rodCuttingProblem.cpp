fd13411e90841fa1a5811c88242912797419746a
Prachi Meshram <prachimesh89@gmail.com>
2023-06-26 11:04:34 +0530

    Rod cutting problem with RECURSION with 1D ARRAY

--- Dynamic Programming/rodCuttingProblem.cpp
+++ Dynamic Programming/rodCuttingProblem.cpp
@@ -57,3 +57,35 @@ int cutRod(vector<int> &price, int n)
 	
 	return fun(price.size() -1, n, price, dp);
 }
+
+
+
+// TABULATION
+
+int cutRod(vector<int> &price, int n)
+{
+	// Write your code here.
+	vector<vector<int>> dp(n, vector<int>(n+1, 0));
+
+	for (int i = 0; i <= n; i++) {
+		dp[0][i] = price[0]*i;
+	}
+	
+	for (int ind = 1; ind < n; ind++) {
+		for (int len = 0; len <= n; len++) {
+
+			int notTake = 0 + dp[ind-1][len];
+
+			int take = INT_MIN;
+			int rodLen = ind+1;
+			if (rodLen <= len) {
+				take = price[ind] + dp[ind][len-rodLen];
+			}
+
+			dp[ind][len] = max(take, notTake);
+		}
+	}
+
+	return dp[n-1][n];
+}
+
