298822ab353abd7d332539aba2bbf00149108595
Prachi Meshram <prachimesh89@gmail.com>
2023-06-27 11:06:14 +0530

    Longest Common Subsequence using SPACE OPTMISATION

--- Dynamic Programming/longestCommonSubsequence.cpp
+++ Dynamic Programming/longestCommonSubsequence.cpp
@@ -62,3 +62,28 @@ int lcs(string s, string t) {
 
   return dp[n][m];
 }
+
+// SPACE OPTIMISATION
+// #include <bits/stdc++.h>
+
+int lcs(string s, string t) {
+  // Write your code here
+  int n = s.size();
+  int m = t.size();
+
+  vector<int> prev(m + 1, 0), curr(m + 1, 0);
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      if (s[i-1] == t[j-1]) {
+        curr[j] = 1 + prev[j - 1];
+      }
+      else {
+        curr[j] = 0 + max(prev[j], curr[j - 1]);
+      }
+    }
+	prev = curr;
+  }
+
+  return prev[m];
+}
