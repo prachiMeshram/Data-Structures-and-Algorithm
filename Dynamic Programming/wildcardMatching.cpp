89987a401157203721a0d31e4e2707db7662f2c2
Prachi Meshram <prachimesh89@gmail.com>
2023-07-06 19:03:33 +0530

    Wildcard Matching TABULATION

--- Dynamic Programming/wildcardMatching.cpp
+++ Dynamic Programming/wildcardMatching.cpp
@@ -106,3 +106,63 @@ public:
         return f(m - 1, n - 1, p, s, dp);
     }
 };
+
+// TABULATION
+class Solution
+{
+
+public:
+    bool isMatch(string s, string p)
+    {
+        string s1 = p;
+        string s2 = s;
+
+        int n = s1.size();
+        int m = s2.size();
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, 0));
+
+        dp[0][0] = true;
+
+        for (int j = 1; j <= m; j++)
+        {
+            dp[0][j] = false;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            int flag = true;
+            for (int k = 1; k <= i; k++)
+            {
+                if (s1[k - 1] != '*')
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[i][0] = flag;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+
+                if (s1[i - 1] == s2[j - 1] || s1[i - 1] == '?')
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if (s1[i - 1] == '*')
+                {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
