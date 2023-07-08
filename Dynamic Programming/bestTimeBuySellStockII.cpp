2f49aceb17950023497bfbb1e0ea50aa0329df59
Prachi Meshram <prachimesh89@gmail.com>
2023-07-08 12:01:25 +0530

    Best Time to Buy and Sell Stock II SPACE OPT

--- Dynamic Programming/bestTimeBuySellStockII.cpp
+++ Dynamic Programming/bestTimeBuySellStockII.cpp
@@ -79,3 +79,29 @@ long getMaximumProfit(long *values, int n)
 
     return dp[0][1];
 }
+
+// SPACE OPT
+
+long getMaximumProfit(long *values, int n)
+{
+    //Write your code here
+    vector<long> ahead(2, 0), curr(2, 0); 
+    ahead[0] = ahead[1] = 0;
+
+    for (int ind = n-1; ind >= 0; ind--) {
+        for (int buy = 0; buy <= 1; buy++) {
+            long profit = 0;
+            if (buy) {
+                profit = max( -values[ind] + ahead[0], 0 + ahead[1]);
+            }
+            else {
+                profit = max( values[ind] + ahead[1], 0 + ahead[0]);
+            }
+
+            curr[buy] = profit;
+        }
+        ahead = curr;
+    }
+
+    return ahead[1];
+}
