bb2fbd3e085de4264a2a80bc518b9078aa27866e
Prachi Meshram <prachimesh89@gmail.com>
2023-07-08 11:30:07 +0530

    Best Time to Buy and Sell Stock II USING DP

--- Dynamic Programming/bestTimeBuySellStockII.cpp
+++ Dynamic Programming/bestTimeBuySellStockII.cpp
@@ -26,3 +26,32 @@ long getMaximumProfit(long *values, int n)
     //Write your code here
     return f(0, 1, values, n);
 }
+
+// USING DP
+
+long f(int ind, int buy, long *values, int n, vector<vector<int>> &dp) {
+    if (ind == n) {
+        return 0;
+    }
+
+    if (dp[ind][buy] != -1) {
+        return dp[ind][buy];
+    }
+
+    long profit = 0;
+    if (buy) {
+        profit = max( -values[ind] + f(ind+1, 0, values, n, dp), 0 + f(ind+1, 1, values, n, dp));
+    }
+    else {
+        profit = max( values[ind] + f(ind+1, 1, values, n, dp), 0 + f(ind+1, 0, values, n, dp));
+    }
+
+    return dp[ind][buy] = profit;
+}
+
+long getMaximumProfit(long *values, int n)
+{
+    //Write your code here
+    vector<vector<int>> dp (n, vector<int> (2, -1)); 
+    return f(0, 1, values, n, dp);
+}
