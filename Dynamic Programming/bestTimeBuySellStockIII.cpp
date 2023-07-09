7015fc669c29ad96a0ce340d2f8b89375e095254
Prachi Meshram <prachimesh89@gmail.com>
2023-07-09 23:10:46 +0530

    Best Time to Buy and Sell Stock III SPACE OPT

--- Dynamic Programming/bestTimeBuySellStockIII.cpp
+++ Dynamic Programming/bestTimeBuySellStockIII.cpp
@@ -76,3 +76,30 @@ public:
         return dp[0][1][2];
     }
 };
+
+// SPACE OPT
+class Solution {
+
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> ahead(2, vector<int> (3, 0));
+        vector<vector<int>> cur(2, vector<int> (3, 0));
+
+        for (int ind = n-1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    if (buy) {
+                        cur[buy][cap] = max( -prices[ind] + ahead[0][cap], 0 + ahead[1][cap]);
+                    }
+                    else {
+                        cur[buy][cap] = max(prices[ind] + ahead[1][cap-1], 0 + ahead[0][cap]);
+                    }      
+                }
+            }
+            ahead = cur;
+        }
+
+        return ahead[1][2];
+    }
+};
