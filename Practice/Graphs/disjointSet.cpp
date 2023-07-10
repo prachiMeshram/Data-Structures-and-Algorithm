2d9a697d0a117cb798a683c574c7e2f77ed420ec
Prachi Meshram <prachimesh89@gmail.com>
2023-07-10 16:48:57 +0530

    Disjoint sets

---
+++ Practice/Graphs/disjointSet.cpp
@@ -0,0 +1,106 @@
+// https://ide.geeksforgeeks.org/392f3f85-cb79-4631-9da2-3dd78e775873
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet
+{
+    vector<int> rank, size, parent;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        size.resize(n + 1, 1);
+        parent.resize(n + 1);
+
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (parent[node] == node)
+        {
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ulp_v = findUPar(v);
+        int ulp_u = findUPar(u);
+        if (ulp_u == ulp_v)
+        {
+            return;
+        }
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_v = findUPar(v);
+        int ulp_u = findUPar(u);
+        if (ulp_u == ulp_v)
+        {
+            return;
+        }
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+int main()
+{
+    DisjointSet ds(7);
+    ds.unionBySize(1, 2);
+    ds.unionBySize(2, 3);
+    ds.unionBySize(4, 5);
+    ds.unionBySize(6, 7);
+    ds.unionBySize(5, 6);
+
+    if (ds.findUPar(3) == ds.findUPar(7))
+    {
+        cout << "same" << endl;
+    }
+    else
+    {
+        cout << "not same" << endl;
+    }
+
+    ds.unionBySize(3, 7);
+
+    if (ds.findUPar(3) == ds.findUPar(7))
+    {
+        cout << "same" << endl;
+    }
+    else
+    {
+        cout << "not same" << endl;
+    }
+
+    return 0;
+}
