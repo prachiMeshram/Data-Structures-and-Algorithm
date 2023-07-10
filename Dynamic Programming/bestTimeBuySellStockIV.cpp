e35b929d2db3d9f10b5c22f862e0bfc412cac33d
Prachi Meshram <prachimesh89@gmail.com>
2023-07-10 07:33:20 +0530

    Best Time to Buy and Sell Stock IV TABULATIO

--- Dynamic Programming/bestTimeBuySellStockIV.cpp
+++ Dynamic Programming/bestTimeBuySellStockIV.cpp
@@ -51,3 +51,26 @@ public:
         return f(0, 0, prices, n, k, dp);
     }
 };
+
+// TABULATION 
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n =  prices.size();
+
+        vector<vector<int>> dp(n+1, vector<int> (2*k+1, 0));
+
+        for (int ind = n-1; ind >= 0; ind--) {
+            for (int transNo = 2*k-1; transNo >= 0; transNo--) {
+                if (transNo % 2 == 0) { // buy
+                    dp[ind][transNo] = max ( -prices[ind] + dp[ind+1][transNo+1], 0 + dp[ind+1][transNo] );
+                }
+                else {
+                    dp[ind][transNo] = max ( prices[ind] + dp[ind+1][transNo+1], 0 + dp[ind+1][transNo] ) ;
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+};
