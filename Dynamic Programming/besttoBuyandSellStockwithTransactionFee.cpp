b97f812090fb9f3b2e930509350b71f897c65952
Prachi Meshram <prachimesh89@gmail.com>
2023-07-12 09:09:57 +0530

    Best Time to Buy and Sell Stock with Transaction Fee MEMOISATION

--- Dynamic Programming/besttoBuyandSellStockwithTransactionFee.cpp
+++ Dynamic Programming/besttoBuyandSellStockwithTransactionFee.cpp
@@ -3,6 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// RECURSION
 class Solution {
 private: 
     int f(int ind, int buy, vector<int>& prices, int fee, int n) {
@@ -21,5 +22,34 @@ public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
         return f(0, 1, prices, fee, n);
+    }
+};
+
+// MEMOISATION
+class Solution {
+private: 
+    int f(int ind, int buy, vector<int>& prices, int fee, int n, vector<vector<int>> dp) {
+        if (ind == n) {
+            return 0;
+        }
+
+        if (dp[ind][buy] != -1) {
+            return dp[ind][buy];
+        }
+
+        if (buy) {
+            return dp[ind][buy] = max( -prices[ind] + f(ind+1, 0, prices, fee, n, dp), 0 + f(ind+1, 1, prices, fee, n, dp));
+        }
+        else {
+            return dp[ind][buy] = max( prices[ind] - fee + f(ind+1, 1, prices, fee, n, dp), 0 + f(ind+1, 0, prices, fee, n, dp));
+        }
+    }
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+
+        vector<vector<int>> dp(n, vector<int> (2, -1));
+
+        return f(0, 1, prices, fee, n, dp);
     }
 };
