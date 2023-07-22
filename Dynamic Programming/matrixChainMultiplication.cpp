2516fb76c76f1570474ef77807bd146db8cea7f4
Prachi Meshram <prachimesh89@gmail.com>
2023-07-22 09:16:06 +0530

    Matrix Chain Multiplication MEMOISATION

--- Dynamic Programming/matrixChainMultiplication.cpp
+++ Dynamic Programming/matrixChainMultiplication.cpp
@@ -22,3 +22,32 @@ int matrixMultiplication(vector<int> &arr, int N)
     // Write your code here.
     return f(1, N-1, arr);
 }
+
+// MEMOISATION
+#include <bits/stdc++.h> 
+
+int f(int i, int j, vector<int> &arr, vector<vector<int>> &dp) {
+    if (i==j) {
+        return 0;
+    }
+
+    if(dp[i][j] != -1) {
+        return dp[i][j];
+    }
+
+    int mini = INT_MAX;
+
+    for (int k = i; k < j; k++) {
+        int steps = f(i, k, arr, dp) + f(k+1, j, arr, dp) + arr[i-1]*arr[k]*arr[j];
+        mini = min(mini, steps);
+    }
+    return dp[i][j] = mini;
+}
+
+int matrixMultiplication(vector<int> &arr, int N)
+{
+    // Write your code here.
+    vector<vector<int>> dp(N, vector<int> (N, -1));
+
+    return f(1, N-1, arr, dp);
+}
