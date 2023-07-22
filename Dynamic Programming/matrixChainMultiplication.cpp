779e63d6f236c603f6cf678e7dffa94a72666117
Prachi Meshram <prachimesh89@gmail.com>
2023-07-22 09:27:02 +0530

    Matrix Chain Multiplication TABULATION

--- Dynamic Programming/matrixChainMultiplication.cpp
+++ Dynamic Programming/matrixChainMultiplication.cpp
@@ -51,3 +51,31 @@ int matrixMultiplication(vector<int> &arr, int N)
 
     return f(1, N-1, arr, dp);
 }
+
+// TABULATION
+
+#include <bits/stdc++.h> 
+
+int matrixMultiplication(vector<int> &arr, int N)
+{
+    // Write your code here.
+    vector<vector<int>> dp(N, vector<int> (N, -1));
+
+    for (int i = 1; i < N; i++) {
+        dp[i][i] = 0; 
+    }
+
+    for (int i = N-1; i > 0; i--) {
+        for (int j = i+1; j < N; j++) {
+            int mini = INT_MAX;
+
+            for (int k = i; k < j; k++) {
+                int steps = dp[i][k] + dp[k+1][j] + arr[i-1]*arr[k]*arr[j];
+                mini = min(mini, steps);
+            }
+            dp[i][j] = mini;
+        }
+    }
+
+    return dp[1][N-1];
+}
