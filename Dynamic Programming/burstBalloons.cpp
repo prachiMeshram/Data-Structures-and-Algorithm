ffec53dc52904ce3754f0938ba2229b6c356e4bd
Prachi Meshram <prachimesh89@gmail.com>
2023-07-25 11:39:01 +0530

    Burst Balloons MEMOISATION

--- Dynamic Programming/burstBalloons.cpp
+++ Dynamic Programming/burstBalloons.cpp
@@ -29,3 +29,35 @@ public:
         return f(1, n, a);
     }
 };
+
+
+// MEMOISATION 
+class Solution {
+private: 
+    int f(int i, int j, vector<int> &a, vector<vector<int>> &dp) {
+        if (i > j) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int maxi = INT_MIN;
+        for (int ind = i; ind <= j; ind++) {
+            int coins = a[i-1]*a[ind]*a[j+1] + f(i, ind-1, a, dp) + f(ind+1, j, a, dp);
+            maxi = max(maxi, coins);
+        }
+        return dp[i][j] = maxi;
+    }
+public:
+    int maxCoins(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> a = nums;
+
+        a.push_back(1);
+        a.insert(a.begin(), 1);
+        vector<vector<int>> dp(n+2, vector<int> (n+2, -1));
+
+        return f(1, n, a, dp);
+    }
+};
