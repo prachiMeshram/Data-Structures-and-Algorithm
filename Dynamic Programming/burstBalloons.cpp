5197af336229df80fba7d04f0c92ca492e13f154
Prachi Meshram <prachimesh89@gmail.com>
2023-07-25 11:42:32 +0530

    Burst Balloons TABULATION

--- Dynamic Programming/burstBalloons.cpp
+++ Dynamic Programming/burstBalloons.cpp
@@ -61,3 +61,35 @@ public:
         return f(1, n, a, dp);
     }
 };
+
+// TABULATION
+
+class Solution {
+
+public:
+    int maxCoins(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> a = nums;
+
+        a.push_back(1);
+        a.insert(a.begin(), 1);
+        vector<vector<int>> dp(n+2, vector<int> (n+2, 0));
+
+        for (int i = n; i >= 1; i-- ) {
+            for (int j = 1; j <= n; j++) {
+                if (i>j) {
+                    continue;
+                }
+                int maxi = INT_MIN;
+                for (int ind = i; ind <= j; ind++) {
+                    int coins = a[i-1]*a[ind]*a[j+1] + dp[i][ind-1]+ dp[ind+1][j];
+                    maxi = max(maxi, coins);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+
+        return dp[1][n];
+    }
+};
