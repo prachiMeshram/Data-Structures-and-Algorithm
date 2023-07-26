da07936d70dbcf2c37a499b7e4032a4556ce4f9a
Prachi Meshram <prachimesh89@gmail.com>
2023-07-26 16:37:46 +0530

    Boolean Evaluation TABULATION

--- Dynamic Programming/booleanEvaluation.cpp
+++ Dynamic Programming/booleanEvaluation.cpp
@@ -122,3 +122,70 @@ int evaluateExp(string & exp) {
 
     return f(0, n-1, 1, exp, dp);
 }
+
+// TABULATION
+
+#include <bits/stdc++.h> 
+
+#define ll long long
+
+int mod = 1000000007;
+
+int evaluateExp(string & exp) {
+    // Write your code here.
+    int n = exp.size();
+
+    vector<vector<vector<ll>>> dp(n, vector<vector<ll>> (n, vector<ll> (2, 0)));
+
+    for (int i = n-1; i >= 0; i--) {
+        for (int j = 0; j < n; j++) {
+            if (i > j) {
+                continue;
+            }
+            for (int isTrue = 0; isTrue <= 1; isTrue++) {
+                if (i == j) {
+                    if (isTrue) dp[i][j][isTrue] = (exp[i] == 'T');
+                    else dp[i][j][isTrue] = (exp[i] == 'F');
+                    continue;
+                }
+                
+                ll nways = 0;
+                for (int k = i + 1; k < j; k += 2 ) {
+
+                    ll lt = dp[i][k-1][1];
+                    ll lf = dp[i][k-1][0];
+                    ll rt = dp[k+1][j][1];
+                    ll rf = dp[k+1][j][0];
+
+                    if (exp[k] == '|') {
+                        if (isTrue) {
+                            nways = (nways + (lt*rf) % mod + (lf*rt)% mod + (lt*rt)% mod) % mod;
+                        }
+                        else {
+                            nways = (nways + (lf*rf)% mod) % mod;
+                        }
+                    }
+                    else if (exp[k] == '&') {
+                        if (isTrue) {
+                            nways = (nways + (lt*rt)% mod) % mod;
+                        }
+                        else {
+                            nways = (nways + (lt*rf)% mod + (lf*rt)% mod + (lf*rf)% mod) % mod;
+                        }
+                    }
+                    else {
+                        if (isTrue) {
+                            nways = (nways + (lt*rf)% mod + (lf*rt)% mod) % mod;
+                        }
+                        else {
+                            nways = (nways + (lt*rt)% mod + (lf*rf)% mod) % mod;
+                        }
+                    }
+                }
+                dp[i][j][isTrue] = nways; 
+            }
+        }
+    }
+
+    return dp[0][n-1][1];
+}
