29e2cee8e4a6d47e2655b4f6e72fe267d63f2584
Prachi Meshram <prachimesh89@gmail.com>
2023-07-28 10:40:53 +0530

    Palindrome Partitioning ll MEMOISATION

---
+++ Dynamic Programming/palindromePartitioningll.cpp
@@ -0,0 +1,45 @@
+// https://www.codingninjas.com/studio/problems/palindrome-partitioning_873266?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+bool isPalindrom(int i, int j, string &str) {
+    while (i < j) {
+        if (str[i] != str[j]) {
+            return false; 
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+int f(int i, string &str, int n, vector<int> dp) {
+
+    if (dp[i] != -1) {
+        return dp[i];
+    }
+
+    if (i == n){
+        return 0;
+    }
+
+    int mini = INT_MAX;
+
+    for (int j = i; j < n; j++) {
+        if(isPalindrom(i, j, str)) {
+            int cost = 1 + f(j+1, str, n, dp);
+            mini = min(mini, cost);
+        }
+    }
+
+    return dp[i] = mini;
+}
+int palindromePartitioning(string str) {
+    // Write your code here
+    int n =str.size();
+
+    vector<int> dp(n+1, -1);
+
+    return f(0, str, n, dp) - 1;
+}
