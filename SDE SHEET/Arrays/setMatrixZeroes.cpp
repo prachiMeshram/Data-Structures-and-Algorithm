d212c61f7336194f93bfe87e236169fbb3a508c1
Prachi Meshram <prachimesh89@gmail.com>
2023-08-03 14:50:36 +0530

    optimum sol with const space

--- SDE SHEET/Arrays/setMatrixZeroes.cpp
+++ SDE SHEET/Arrays/setMatrixZeroes.cpp
@@ -76,3 +76,44 @@ public:
         }
     }
 };
+
+// optimum solution
+class Solution {
+
+public:
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int col0 = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(matrix[i][j] == 0 ) {
+                    if (j == 0) {
+                        col0 = 0;
+                        matrix[i][0] = 0;
+                    }
+                    else {
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+                }
+            }
+        }
+
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = m-1; j >= 0; j--) {
+                if (j == 0) {
+                    if(col0 == 0) {
+                    matrix[i][j] = 0;
+                }
+                }
+                else if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
