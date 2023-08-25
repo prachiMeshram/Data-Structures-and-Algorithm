75f6fa1cd72d0f65d2654677daa1459e50bc1cf6
Prachi Meshram <prachimesh89@gmail.com>
2023-08-25 07:18:44 +0530

    KADANE'S ALGO to print max sum subarray

--- SDE SHEET/Arrays/KADANESAlgoMaximumSubarraySum.cpp
+++ SDE SHEET/Arrays/KADANESAlgoMaximumSubarraySum.cpp
@@ -29,3 +29,46 @@ long long maxSubarraySum(int arr[], int n)
     }
     
 }
+
+// to print subarray of maximum sum
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    /*
+        Don't write main().
+        Don't read input, it is passed as function argument.    
+        No need to print anything.
+        Taking input and printing output is handled automatically.
+    */
+    long long maxi = LONG_MIN;
+    long long sum = 0;
+
+    int start = 0;
+    int ansStart = -1, ansEnd = -1;
+
+    for (int i = 0; i < n; i++) {
+
+        if (sum == 0) {
+            start = i; // starting index
+        }
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+
+            ansStart = start;
+            ansEnd = i;
+        }
+        
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    cout << "the subarray is: [";
+    for (int i = ansStart; i <= ansEnd; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "]n";
+}
