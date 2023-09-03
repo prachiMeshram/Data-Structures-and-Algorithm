4d07ae2f3f7cb527daa0b10b6c776e01b5d73b15
Prachi Meshram <prachimesh89@gmail.com>
2023-09-03 21:58:44 +0530

    Search In A 2D Matrix my solution

---
+++ SDE SHEET/ArraysII/searchInA2DMatrix.cpp
@@ -0,0 +1,28 @@
+// https://www.codingninjas.com/studio/problems/search-in-a-2d-matrix_980531?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& mat, int target) {   
+    int n = mat.size();
+    int m = mat[0].size();
+
+    int s = 0;
+    int e = n*m - 1;
+    int mid = s + (e-s)/2;
+
+    while (s <= e) {
+        if (mat[mid/m][mid%m] == target) {
+            return true;
+        }
+        else if (mat[mid/m][mid%m]< target) {
+            s = mid+1;
+        }
+        else {
+            e = mid-1;
+        }
+        mid = s + (e-s)/2;
+    }
+
+    return false;
+}
