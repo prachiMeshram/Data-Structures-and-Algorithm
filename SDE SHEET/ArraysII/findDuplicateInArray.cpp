6ec4d2e8e693ae10964285f465a7d95675b77aae
Prachi Meshram <prachimesh89@gmail.com>
2023-09-03 14:39:54 +0530

    linkedlist detect cycle approach

--- SDE SHEET/ArraysII/findDuplicateInArray.cpp
+++ SDE SHEET/ArraysII/findDuplicateInArray.cpp
@@ -3,14 +3,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findDuplicate(vector<int> &arr, int n){
-	// Write your code here.
-	
-	sort(arr.begin(), arr.end());
-	
-	for (int i = 0; i < n; i++) {
-		if (arr[i] == arr[i+1]) {
-			return arr[i];
-		}
-	}
+int findDuplicate(vector<int> &arr, int n)
+{
+    // Write your code here.
+
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == arr[i + 1])
+        {
+            return arr[i];
+        }
+    }
+}
+
+// linkedlist detect cycle approach
+
+int findDuplicate(vector<int> &arr, int n)
+{
+    // Write your code here.
+    int slow = 0;
+    int fast = 0;
+
+    do
+    {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while (slow != fast);
+
+    slow = 0;
+
+    while (slow != fast)
+    {
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+
+    return slow;
 }
