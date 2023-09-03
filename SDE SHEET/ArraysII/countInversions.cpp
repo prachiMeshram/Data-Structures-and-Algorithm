93baeb7b68a769d2b1385122abb650bd1882accb
Prachi Meshram <prachimesh89@gmail.com>
2023-09-03 19:50:22 +0530

    Count Inversions using MERGE SORT

--- SDE SHEET/ArraysII/countInversions.cpp
+++ SDE SHEET/ArraysII/countInversions.cpp
@@ -17,3 +17,54 @@ long long getInversions(long long *arr, int n){
 
     return ans;
 }
+
+// using merge sort
+
+#include <bits/stdc++.h> 
+
+long long merge(long long *arr, int s, int e) {
+    int cnt = 0;
+    
+    int m =  s + (e - s)/2;
+    vector<int> temp;
+    
+    int l = s;
+    int r = m+1;
+    
+    while (l <= m && r <= e) {
+        if (arr[l] <= arr[r]) {
+            temp.push_back(arr[l++]);
+        }
+        else {
+            temp.push_back(arr[r++]);
+            cnt += m-l+1;
+        }
+    }
+
+    while (l <= m) {
+        temp.push_back(arr[l++]);
+    }
+    while (r <= e) {
+        temp.push_back(arr[r++]);
+    }
+    
+    for (int i = s; i <= e; i++) {
+        arr[i] = temp[i-s];
+    }
+    return cnt;
+}
+
+long long mergeSort(long long *arr, int s, int e) {
+    long long cnt = 0;
+    if (s >= e) return cnt;
+    int mid =  s + (e - s)/2;
+    cnt += mergeSort(arr, s, mid);
+    cnt += mergeSort(arr, mid+1, e);
+    cnt += merge(arr, s, e);
+
+    return cnt;
+}
+long long getInversions(long long *arr, int n){
+    // Write your code here.
+    return mergeSort(arr, 0, n-1);
+}
