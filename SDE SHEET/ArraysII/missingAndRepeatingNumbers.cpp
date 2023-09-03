06ff4d505e85dceca285cbb38671784ea54499a9
Prachi Meshram <prachimesh89@gmail.com>
2023-09-03 16:26:54 +0530

    using maths

--- SDE SHEET/ArraysII/missingAndRepeatingNumbers.cpp
+++ SDE SHEET/ArraysII/missingAndRepeatingNumbers.cpp
@@ -25,3 +25,33 @@ pair<int,int> missingAndRepeating(vector<int> &arr, int n)
 
 	return ans;
 }
+
+// using maths 
+#include <bits/stdc++.h>
+
+pair<int,int> missingAndRepeating(vector<int> &arr, int N)
+{
+	// Write your code here 
+	// pair<int,int> ans;
+	
+	long long n = arr.size();
+	
+	long long Sn = (n*(n+1))/2;
+	long long S2n = (n*(n+1)*(2*n+1))/6;
+	
+	long long S = 0;
+	long long S2 = 0;
+	for (auto it: arr) {
+		S += it;
+		S2 += (long long)it*(long long)it;
+	}
+	
+	long long val1 = S-Sn; // x-y 
+	long long val2 = S2-S2n;
+	val2 = val2/val1; // x+y
+	
+	long long x = (val1+val2)/2;
+	long long y = x - val1;
+
+	return {(int)y, (int)x};
+}
