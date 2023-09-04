35494a53cba972fb8193644a21368188075cd65c
Prachi Meshram <prachimesh89@gmail.com>
2023-09-04 16:58:56 +0530

    Reverse Pairs using merge sort

--- SDE SHEET/ArraysIII/reversePairs.cpp
+++ SDE SHEET/ArraysIII/reversePairs.cpp
@@ -17,3 +17,68 @@ int reversePairs(vector<int> &arr, int n){
 
 	return count;
 }
+
+// merge sort
+#include <bits/stdc++.h> 
+
+void merge(vector<int> &arr, int s, int e) {
+	int m = s + (e-s)/2;
+	vector<int> temp;
+	int l = s;
+	int r = m+1;
+
+	while (l <= m && r <= e) {
+		if (arr[r] < arr[l]) {
+			temp.push_back(arr[r++]);
+		}
+		else {
+			temp.push_back(arr[l++]);
+		}
+	}
+
+	while (l <= m) {
+		temp.push_back(arr[l++]);
+	}
+	while (r <= e) {
+		temp.push_back(arr[r++]);
+	}
+
+	for (int i = s; i <= e; i++) {
+		arr[i] = temp[i-s];
+	}
+}
+
+int countRevPairs(vector<int> &arr, int s, int e) {
+	int cnt = 0;
+	int m = s + (e-s)/2;
+	int r = m+1;
+	
+	for (int i = s; i <= m; i++) {
+		while (r <= e && arr[i] > 2 * arr[r]) {
+			r++;
+		} 
+		cnt += (r-(m+1));
+	}
+
+	return cnt;
+}
+
+int mergeSort(vector<int> &arr, int s, int e) {
+	int cnt = 0;
+	if (s >= e) {
+		return cnt;
+	}
+	int m = s + (e-s)/2;
+	cnt += mergeSort(arr, s, m); 
+	cnt += mergeSort(arr, m+1, e);
+	cnt += countRevPairs(arr, s, e);
+	merge(arr, s, e);
+	
+	return cnt;
+}
+
+int reversePairs(vector<int> &arr, int n){
+	// Write your code here.	
+	
+	mergeSort(arr, 0, n-1);
+}
