ab8e9dc215bada260dd3377d13ef63bbc0db7651
Prachi Meshram <prachimesh89@gmail.com>
2023-09-04 20:00:42 +0530

    4Sum using optimal approach

--- SDE SHEET/ArraysIV/4Sum.cpp
+++ SDE SHEET/ArraysIV/4Sum.cpp
@@ -30,3 +30,43 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
     vector<vector<int>> ans(st.begin(), st.end());
     return ans;
 }
+
+// optimal sol
+#include<bits/stdc++.h>
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    // Write your code here
+    
+    int n = nums.size();
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    
+    for (int i = 0; i < n; i++) {
+        if (i > 0 && nums[i] == nums[i-1]) continue;
+        for (int j = i+1; j < n; j++) {
+            if (j > i+1 && nums[j] == nums[j-1]) continue;
+            int k = j+1;
+            int l = n-1;
+            while (k < l) {
+                long long sum = nums[i] + nums[j];
+                sum += nums[k] + nums[l];
+                if ( sum == target) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                    ans.push_back(temp);
+                    k++;
+                    l--;
+                    while (nums[k] != nums[k-1]) k++;
+                    while (nums[l] != nums[l+1]) l--;
+                }
+                else if ( sum < target) {
+                    k++;
+                }
+                else {
+                    l--;
+                }
+            }
+        }
+    }
+    
+    return ans;
+}
