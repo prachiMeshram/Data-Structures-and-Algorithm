54e1a08379712f8731eea6bba381c60c97cda986
Prachi Meshram <prachimesh89@gmail.com>
2023-09-10 11:51:28 +0530

    Add Two Numbers (overflow issue)

--- SDE SHEET/LinkedList/addTwoNumbers.cpp
+++ SDE SHEET/LinkedList/addTwoNumbers.cpp
@@ -27,9 +27,8 @@ public:
     }
 };
 
-
-    // optimal approach
-    Node *addTwoNumbers(Node *num1, Node *num2)
+// optimal approach
+Node *addTwoNumbers(Node *num1, Node *num2)
 {
     // Write your code here.
     Node *l1 = num1;
@@ -61,4 +60,55 @@ public:
         temp = temp->next;
     }
     return dummy->next;
+}
+
+
+// other approach but not encouraged due to overflow
+Node *addTwoNumbers(Node *num1, Node *num2)
+{
+    // Write your code here.
+    if (num1 == NULL) return num2; 
+    if (num2 == NULL) return num1; 
+
+    long long n1 = 0;
+    long long n2 = 0;
+
+    Node* temp = num1;
+    int p = 0;
+    while (temp != nullptr) {
+        n1 += temp->data*pow(10, p);
+        temp = temp->next;
+        p++;
+    }
+
+    temp = num2;
+    p = 0;
+    while (temp != nullptr) {
+        n2 += temp->data*pow(10, p);
+        temp = temp->next;
+        p++;
+    }
+
+    long long n3 = n1 + n2;
+
+    Node* newH = nullptr;
+    Node* current = nullptr;
+    if (n3 == 0) {
+        return new Node(0); // Special case for sum equal to zero.
+    }
+
+    while (n3) {
+        int digit = n3 % 10;
+        n3 /= 10;
+        Node* node = new Node(digit);
+        if (newH == nullptr) {
+            newH = node;
+            current = node;
+        } else {
+            current->next = node;
+            current = node;
+        }
+    }
+
+    return newH;
 }
