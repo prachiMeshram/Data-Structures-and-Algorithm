055f752bad360777e0634942b87f8c2f48224cd4
Prachi Meshram <prachimesh89@gmail.com>
2023-09-13 16:28:08 +0530

    Rotate Linked List optimised approach

---
+++ SDE SHEET/LinkedList & Arrays/rotateLinkedList.cpp
@@ -0,0 +1,37 @@
+// https://www.codingninjas.com/studio/problems/920454?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// * Definition for singly-linked list.
+ class Node {
+ public:
+     int data;
+     Node *next;
+     Node() : data(0), next(nullptr) {}
+     Node(int x) : data(x), next(nullptr) {}
+     Node(int x, Node *next) : data(x), next(next) {}
+ };
+
+
+Node *rotate(Node *head, int k) {
+     // Write your code here.
+     
+     Node* temp = head;
+     int cnt = 1;
+     
+     while (temp->next != NULL) {
+          temp = temp->next;
+          cnt++;
+     }
+     
+     k = k%cnt;
+     temp->next = head;
+     int n = cnt-k;
+     while (n--) {
+          temp = temp->next;
+     }
+     head = temp->next;
+     temp->next = NULL;
+     return head; 
+}
