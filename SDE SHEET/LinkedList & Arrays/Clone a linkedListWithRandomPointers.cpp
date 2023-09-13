717ebfd52069d70beb18fba506ba288504683acf
Prachi Meshram <prachimesh89@gmail.com>
2023-09-13 19:54:11 +0530

    Clone a Linked List with random pointers OPTIMISED APPROACH

--- SDE SHEET/LinkedList & Arrays/Clone a linkedListWithRandomPointers.cpp
+++ SDE SHEET/LinkedList & Arrays/Clone a linkedListWithRandomPointers.cpp
@@ -14,7 +14,8 @@ using namespace std;
 		Node(int x, Node *next, Node *random) : data(x), next(next), random(random) {}
  };
 
-#include<bits/stdc++.h>
+
+// USING HASHING
 
 Node *cloneLL(Node *head){
 	// Write your code here
@@ -35,4 +36,38 @@ Node *cloneLL(Node *head){
 		temp = temp->next;
 	}
 	return mpp[head];
+}
+
+// OPTIMISED APPROACH
+Node *cloneLL(Node *head){
+	// Write your code here
+	Node* temp = head;
+	
+	while (temp != NULL) {
+		Node* newNode = new Node(temp->data);
+		newNode->next = temp->next;
+		temp->next = newNode;
+		temp = temp->next->next;
+	}
+	
+	Node* f = head;
+	while (f != NULL) {
+		if (f->random != NULL) {
+			f->next->random = f->random->next;
+		}
+		f = f->next->next;
+	}
+	Node* dummy = new Node(-1);
+	f = head;
+	temp = dummy;
+	Node* s;
+
+	while (f != NULL) {
+		s = f->next->next;
+		temp->next = f->next;
+		f->next = s;
+		temp = temp->next;
+		f = s;
+	}
+	return dummy->next;
 }
