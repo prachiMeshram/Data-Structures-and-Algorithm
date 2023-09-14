99a8afa3f816a1e85e96e85218e839289113e7df
Prachi Meshram <prachimesh89@gmail.com>
2023-09-14 08:16:36 +0530

    3Sum BRUTE FORE SOL

--- SDE SHEET/LinkedList & Arrays/3Sum.cpp
+++ SDE SHEET/LinkedList & Arrays/3Sum.cpp
@@ -3,6 +3,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// BRUTE FORCE
+vector<vector<int>> triplet(int n, vector<int> &arr)
+{
+    set<vector<int>> st;
+
+    // check all possible triplets:
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (arr[i] + arr[j] + arr[k] == 0)
+                {
+                    vector<int> temp = {arr[i], arr[j], arr[k]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
+        }
+    }
+
+    // store the set elements in the answer:
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
 // BETTER APPROACH
 #include <bits/stdc++.h>
 vector<vector<int>> findTriplets(vector<int> arr, int n, int K)
