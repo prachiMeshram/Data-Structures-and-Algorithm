e6a42009cee2e304d2531678c2ce725cf4abf543
Prachi Meshram <prachimesh89@gmail.com>
2023-09-14 08:15:21 +0530

    3Sum BETTER SOL

--- SDE SHEET/LinkedList & Arrays/3Sum.cpp
+++ SDE SHEET/LinkedList & Arrays/3Sum.cpp
@@ -1,37 +1,76 @@
 // https://www.codingninjas.com/studio/problems/893028?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
 
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
 
+// BETTER APPROACH
+#include <bits/stdc++.h>
+vector<vector<int>> findTriplets(vector<int> arr, int n, int K)
+{
+    // Write your code here.
+    set<vector<int>> st;
+
+    for (int i = 0; i < n; i++)
+    {
+        set<int> hashset;
+        for (int j = i + 1; j < n; j++)
+        {
+            // Calculate the 3rd element:
+            int third = K - (arr[i] + arr[j]);
+
+            // Find the element in the set:
+            if (hashset.find(third) != hashset.end())
+            {
+                vector<int> temp = {arr[i], arr[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            }
+            hashset.insert(arr[j]);
+        }
+    }
+
+    // store the set in the answer:
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
 // OPTIMAL APPROACH
-vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
-	// Write your code here.
+vector<vector<int>> findTriplets(vector<int> arr, int n, int K)
+{
+    // Write your code here.
     vector<vector<int>> ans;
     sort(arr.begin(), arr.end());
-    
-    for (int i = 0; i < n; i++) {
-        if (i > 0 && arr[i] == arr[i-1]) continue;
-        int l = i+1;
-        int r = n-1;
-        while (l < r) {
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0 && arr[i] == arr[i - 1])
+            continue;
+        int l = i + 1;
+        int r = n - 1;
+        while (l < r)
+        {
             int sum = arr[i] + arr[l] + arr[r];
-            if (sum == K) {
+            if (sum == K)
+            {
                 vector<int> temp = {arr[i], arr[l], arr[r]};
                 ans.push_back(temp);
                 l++;
                 r--;
-                while (arr[l] == arr[l-1]) l++;
-                while (arr[r] == arr[r+1]) r--;
+                while (arr[l] == arr[l - 1])
+                    l++;
+                while (arr[r] == arr[r + 1])
+                    r--;
             }
-            else if (sum < K) {
-                l++; 
+            else if (sum < K)
+            {
+                l++;
             }
-            else {
+            else
+            {
                 r--;
             }
         }
     }
-    
+
     return ans;
-    
 }
