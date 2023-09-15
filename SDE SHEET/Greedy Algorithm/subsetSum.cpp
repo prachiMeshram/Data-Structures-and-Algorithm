5a0c0779a623dc2632ec0ba76474f105b1f154c9
Prachi Meshram <prachimesh89@gmail.com>
2023-09-15 20:32:19 +0530

    Subset Sum using RECURSION

---
+++ SDE SHEET/Greedy Algorithm/subsetSum.cpp
@@ -0,0 +1,23 @@
+// https://www.codingninjas.com/studio/problems/subset-sum_3843086?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int i, vector<int> &nums, vector<int> &ans, int sum) {
+	if (i == nums.size()) {
+		ans.push_back(sum);
+		return;
+	}
+	// pick
+	solve(i+1, nums, ans, sum+nums[i]);
+	// not pick
+	solve(i+1, nums, ans, sum);
+}
+
+vector<int> subsetSum(vector<int> &nums){
+	// Write your code here.
+	vector<int> ans;
+	solve(0, nums, ans, 0);
+	sort(ans.begin(), ans.end());
+	return ans;
+}
