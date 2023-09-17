143cbdcf70cdcb9aec49b2b098ce7fb12e44c045
Prachi Meshram <prachimesh89@gmail.com>
2023-09-17 17:54:00 +0530

    Print Permutations - String using RECURSION

--- SDE SHEET/RecursionAndBacktracking/printPermutations-String.cpp
+++ SDE SHEET/RecursionAndBacktracking/printPermutations-String.cpp
@@ -3,6 +3,35 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+// Using Recursion
+
+void solve (string &st, vector<string> &ans, vector<int> &mpp, string &s) {
+    if (st.size() == s.size()) {
+        ans.push_back(st);
+        return;
+    }
+    
+    for (int i = 0; i < s.size(); i++) {
+        if (!mpp[i]) {
+            mpp[i] = 1;
+            st.push_back(s[i]);
+            solve(st, ans, mpp, s);
+            st.pop_back();
+            mpp[i] = 0;
+        }
+    }
+}
+vector<string> findPermutations(string &s) {
+    // Write your code here.
+    vector<string> ans;
+    string st;
+    int n = s.size();
+    vector<int> mpp(n, 0);
+    solve (st, ans, mpp, s);
+    
+    return ans;
+}
+
 // using backtracking
 void solve (int ind, string &s, vector<string> &res) {
     if (ind == s.size()) {
