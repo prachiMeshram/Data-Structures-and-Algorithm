180733bae8acae0a94d95322a19fbbd08449b8f6
Prachi Meshram <prachimesh89@gmail.com>
2023-09-17 16:01:38 +0530

    K-th Permutation Sequence OPTIMISED APPROACH

---
+++ SDE SHEET/Recursion/K-thPermutationSequence.cpp
@@ -0,0 +1,30 @@
+// https://www.codingninjas.com/studio/problems/1112626?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// OPTIMISED APPROACH
+string kthPermutation(int n, int k) {
+    // Write your code here.
+    int fact = 1;
+    vector<int> nums;
+    for (int i = 1; i < n; i++) {
+        fact *= i;
+        nums.push_back(i); 
+    }
+    nums.push_back(n);
+    string ans = "";
+    k = k-1;
+
+    while (true) {
+        ans += to_string(nums[k/fact]);
+        nums.erase(nums.begin() + k/fact);
+        if (nums.size() == 0) {
+            break;
+        } 
+        
+        k = k%fact;
+        fact = fact/nums.size();
+    }
+    return ans;
+}
