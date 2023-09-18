dda36a822f082274a5a42aac487431f2c9d422f6
Prachi Meshram <prachimesh89@gmail.com>
2023-09-18 18:43:57 +0530

    M-Coloring Problem

---
+++ SDE SHEET/RecursionAndBacktracking/M-ColoringProblem.cpp
@@ -0,0 +1,40 @@
+// https://www.codingninjas.com/studio/problems/m-coloring-problem_981273?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+bool isPossible(int node, vector<int> &color, vector<vector<int>> &mat, int col, int n) {
+
+    for (int k = 0; k < n; k++) {
+        if (k != node && mat[k][node] == 1 && color[k] == col) {
+            return false;
+        }
+    }
+    return true;
+}
+bool solve(int node, vector<int> &color, vector<vector<int>> &mat, int m, int n) {
+    if (node == n) {
+        return true;
+    }
+
+    for (int i = 1; i <= m; i++) {
+        if (isPossible(node, color, mat, i, n)) {
+            color[node] = 1; 
+            if (solve( node+1, color, mat, m, n)) {
+                return true;
+            }
+            color[node] = 0;
+        }
+    }
+    return false;
+}
+string graphColoring(vector<vector<int>> &mat, int m) {
+    // Write your code here
+    int n = mat.size();
+    vector<int> color(n, 0);
+
+    if (solve(0, color, mat, m, n)) {
+        return "YES";
+    }
+    return "NO";
+}
--- SDE SHEET/RecursionAndBacktracking/sudokuSolver.cpp
+++ SDE SHEET/RecursionAndBacktracking/sudokuSolver.cpp
@@ -1,2 +1,60 @@
 // https://www.codingninjas.com/studio/problems/sudoku-solver_8416969?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
 
+#include <bits/stdc++.h> 
+using namespace std;
+
+bool isSafe (int board[][9], int row, int col, int digit) {
+    
+    for (int i = 0; i < 9; i++) {
+        if (board[row][i] == digit || board[i][col] == digit) {
+            return false;
+        }
+    }
+
+    int sr = (row/3) * 3;
+    int sc = (col/3) * 3;
+
+    for (int i = sr; i < sr+3 ; i++) {
+        for (int j = sc; j < sc+3; j++) {
+            if (board[i][j] == digit) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+bool solve (int board[][9], int row, int col) {
+    // base case 
+    if (row == 9) {
+        return true;
+    }
+    
+    int newR = row, newC = col+1;
+    if (col+1 == 9) {
+        newR = row+1;
+        newC = 0;
+    }
+
+    if (board[row][col] != 0) {
+        return solve(board, newR, newC);
+    }
+    
+    for (int digit = 1; digit <= 9; digit++) {
+        if (isSafe(board, row, col, digit)) {
+            board[row][col] = digit;
+            if (solve(board, newR, newC)) {
+                return true;
+            }
+            board[row][col] = 0;
+        }
+    }
+    
+    return false;
+}
+bool sudokuSolver(int board[][9]) {
+    /* Don't write main().
+     *  Don't read input, it is passed as function argument.
+     *  Don't print output and return output as specified in the question
+    */
+    return solve (board, 0, 0);
+}
