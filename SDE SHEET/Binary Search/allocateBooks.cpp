d66376f4bc66ac7bd14877129eecd5d137ed3582
Prachi Meshram <prachimesh89@gmail.com>
2023-09-20 11:44:54 +0530

    Allocate Books brute force

--- SDE SHEET/Binary Search/allocateBooks.cpp
+++ SDE SHEET/Binary Search/allocateBooks.cpp
@@ -3,6 +3,7 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+// OPTIMAL APPROACH
 bool isPossible(long long mid, vector<int> &time, int n, int m) {
     int dayCount = 1; 
     long long timeSum = 0;
@@ -46,5 +47,47 @@ long long ayushGivesNinjatest(int n, int m, vector<int> time)
         }
     }
 
+    return ans;
+}
+
+// BRUTE FORCE
+#include <bits/stdc++.h> 
+bool isPossible(long long mid, vector<int> &time, int n, int m) {
+    int dayCount = 1; 
+    long long timeSum = 0;
+
+    for (int i = 0; i < m; i++) {
+        if (timeSum + time[i] <= mid) {
+            timeSum += time[i];
+        }
+        else {
+            dayCount++;
+            if (dayCount > n || time[i] > mid) {
+                return false;
+            }
+            timeSum = time[i];
+        }
+    }
+
+    return true;
+}
+long long ayushGivesNinjatest(int n, int m, vector<int> time) 
+{	
+	// Write your code here.
+    long long ans = -1;
+    long long s = 0;
+    long long e = 0;
+
+    for (int i = 0; i < m; i++) {
+        e += time[i];
+        s = max((long long)time[i], s);
+    }
+
+    for (int i = s; i <= e; i++) {
+        if (isPossible (i, time, n, m)) {
+            return i; 
+        }
+    }
+    
     return ans;
 }
