fbcec60aa9077ecf625b6cb3e31023e5f1238926
Prachi Meshram <prachimesh89@gmail.com>
2023-09-21 09:25:46 +0530

    Queue Using two Stacks where push operation is O(1)

--- SDE SHEET/Stack & Queue/queueUsingStack.cpp
+++ SDE SHEET/Stack & Queue/queueUsingStack.cpp
@@ -3,6 +3,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Using two Stacks where push operation is O(1)
+
+class Queue {
+    // Define the data members(if any) here.
+    stack<int> input, output;
+    
+    public:
+    Queue() {
+        // Initialize your data structure here.
+    }
+
+    void enQueue(int val) {
+        // Implement the enqueue() function.
+        input.push(val);
+    }
+
+    int deQueue() {
+        // Implement the dequeue() function.
+        
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        if (output.empty()) return -1;
+        int val = output.top();
+        output.pop();
+        return val;
+    }
+
+    int peek() {
+        // Implement the peek() function here.
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        if (output.empty()) return -1;
+        return output.top();
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function here.
+        if (input.empty() && output.empty()) return true;
+        return false;
+    }
+};
+
 
 // Using two Stacks where push operation is O(N)
 class Queue {
