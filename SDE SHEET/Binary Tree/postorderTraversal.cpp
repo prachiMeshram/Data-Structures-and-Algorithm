177fc0a5ecd96d487c3be20701f2e26fe9a87161
Prachi Meshram <prachimesh89@gmail.com>
2023-09-21 11:46:58 +0530

    Postorder Traversal using RECURSION

--- SDE SHEET/Binary Tree/postorderTraversal.cpp
+++ SDE SHEET/Binary Tree/postorderTraversal.cpp
@@ -3,7 +3,6 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-
 // Following is Binary Tree Node structure:
 class TreeNode
 {
@@ -14,6 +13,22 @@ public:
     TreeNode(int x) : data(x), left(NULL), right(NULL) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
 };
+
+// USING RECURSION
+void pot(TreeNode *root, vector<int> &ans) {
+    if (root == NULL) return;
+
+    pot(root->left, ans);
+    pot(root->right, ans);
+    ans.push_back(root->data);
+}
+vector<int> getPostOrderTraversal(TreeNode *root)
+{
+    // Write your code here.
+    vector<int> ans;
+    pot(root, ans);
+    return ans;
+}
 
 //  Using two stacks
 vector<int> getPostOrderTraversal(TreeNode *root)
