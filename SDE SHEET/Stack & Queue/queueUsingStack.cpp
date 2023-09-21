6a0d5c45b2a7e1a451716e1b6f88047dcb810e35
Prachi Meshram <prachimesh89@gmail.com>
2023-09-21 08:13:12 +0530

    Queue Using Stacks where push operation is O(N)

---
+++ SDE SHEET/Stack & Queue/queueUsingStack.cpp
@@ -0,0 +1,56 @@
+// https://www.codingninjas.com/studio/problems/799482?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Using two Stacks where push operation is O(N)
+class Queue {
+    // Define the data members(if any) here.
+    stack<int> input;
+    stack<int> output;
+    
+    public:
+    Queue() {
+        // Initialize your data structure here.
+    }
+
+    void enQueue(int val) {
+        // Implement the enqueue() function.
+        
+        while (!input.empty()) {
+            output.push(input.top());
+            input.pop();
+            
+        }
+        input.push(val);
+        while (!output.empty()) {
+            input.push(output.top());
+            output.pop();
+        }
+    }
+
+    int deQueue() {
+        // Implement the dequeue() function.
+        if (input.empty()) return -1;
+        
+        int val = input.top();
+        input.pop();
+        return val;
+    }
+
+    int peek() {
+        // Implement the peek() function here.
+        if (input.empty()) return -1;
+    
+        return input.top();
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function here.
+        if (input.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+};
