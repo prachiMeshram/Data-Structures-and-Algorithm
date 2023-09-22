895454253ffe3e151e24d315f4f55f867346ec8c
Prachi Meshram <prachimesh89@gmail.com>
2023-09-22 09:38:40 +0530

    Inorder Traversal MORRIS PREORDER TRAVERSAL

--- SDE SHEET/Binary Tree/inorderTraversal.cpp
+++ SDE SHEET/Binary Tree/inorderTraversal.cpp
@@ -1,10 +1,9 @@
 // https://www.codingninjas.com/studio/problems/inorder-traversal_3839605?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-    // Following is Binary Tree Node structure:
+// Following is Binary Tree Node structure:
 class TreeNode
 {
 public:
@@ -15,28 +14,67 @@ public:
     TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
 };
 
-// MORRIS TRAVERSAL
-vector<int> getInOrderTraversal(TreeNode *root)
+// MORRIS PREORDER TRAVERSAL
+vector<int> getPreOrderTraversal(TreeNode *root)
 {
     // Write your code here.
+    // USING MORRIS TRAVERSAL
     vector<int> ans;
     TreeNode* curr = root;
-    
+
     while (curr != NULL) {
         if (curr->left == NULL) {
             ans.push_back(curr->data);
             curr = curr->right;
         }
         else {
-            TreeNode* prev = curr->left;;
+            TreeNode* prev = curr->left;
             while (prev->right != NULL && prev->right != curr) {
                 prev = prev->right;
             }
             if (prev->right == NULL) {
                 prev->right = curr;
+                ans.push_back(curr->data);
                 curr = curr->left;
             }
             else {
+                prev->right = NULL;
+                curr = curr->right;
+            }
+        }
+    }
+    return ans;
+}
+
+// MORRIS INORDER TRAVERSAL
+vector<int> getInOrderTraversal(TreeNode *root)
+{
+    // Write your code here.
+    vector<int> ans;
+    TreeNode *curr = root;
+
+    while (curr != NULL)
+    {
+        if (curr->left == NULL)
+        {
+            ans.push_back(curr->data);
+            curr = curr->right;
+        }
+        else
+        {
+            TreeNode *prev = curr->left;
+            ;
+            while (prev->right != NULL && prev->right != curr)
+            {
+                prev = prev->right;
+            }
+            if (prev->right == NULL)
+            {
+                prev->right = curr;
+                curr = curr->left;
+            }
+            else
+            {
                 prev->right = NULL;
                 ans.push_back(curr->data);
                 curr = curr->right;
@@ -53,16 +91,20 @@ vector<int> getInOrderTraversal(TreeNode *root)
 {
     // Write your code here.
     vector<int> ans;
-    stack<TreeNode*> st;
+    stack<TreeNode *> st;
     TreeNode *curr = root;
-    
-    while (true) {
-        if (curr != NULL) {
+
+    while (true)
+    {
+        if (curr != NULL)
+        {
             st.push(curr);
             curr = curr->left;
         }
-        else {
-            if (st.empty()) break;
+        else
+        {
+            if (st.empty())
+                break;
             curr = st.top();
             ans.push_back(curr->data);
             st.pop();
@@ -75,13 +117,15 @@ vector<int> getInOrderTraversal(TreeNode *root)
 
 // RECURSIVE APPROACH
 
-void iotraversal(TreeNode *root, vector<int> &ans) {
-    if (root == NULL ) {
+void iotraversal(TreeNode *root, vector<int> &ans)
+{
+    if (root == NULL)
+    {
         return;
     }
-    iotraversal( root->left, ans);
+    iotraversal(root->left, ans);
     ans.push_back(root->data);
-    iotraversal( root->right, ans);
+    iotraversal(root->right, ans);
 }
 vector<int> getInOrderTraversal(TreeNode *root)
 {
