c5d2b6367969f653affbf87fd4e437b0f49a789a
Prachi Meshram <prachimesh89@gmail.com>
2023-09-25 14:18:45 +0530

    Level Order Traversal

---
+++ SDE SHEET/Binary Tree II/levelOrderTraversal.cpp
@@ -0,0 +1,45 @@
+// https://www.codingninjas.com/studio/problems/zigzag-binary-tree-traversal_920532?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+    // Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+
+
+vector<int> levelOrder(TreeNode<int> * root){
+    // Write your code here.
+    vector<int> ans;
+    if (root == NULL) return ans;
+    queue<TreeNode<int> *> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode<int> *node = q.front();
+        q.pop();
+        ans.push_back(node->data);
+        if (node->left) {
+            q.push(node->left);
+        }
+        if (node->right) {
+            q.push(node->right);
+        }
+    }
+    return ans;
+}
