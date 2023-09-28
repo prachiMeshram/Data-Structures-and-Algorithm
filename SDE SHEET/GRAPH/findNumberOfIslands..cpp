5cea38b65cc9ad25167664bc3e6223ff4ef8104c
Prachi Meshram <prachimesh89@gmail.com>
2023-09-28 14:35:06 +0530

    Find Number Of Islands using DFS

--- SDE SHEET/GRAPH/findNumberOfIslands..cpp
+++ SDE SHEET/GRAPH/findNumberOfIslands..cpp
@@ -3,6 +3,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// USING DFS
+void dfs (int r, int c, vector<vector<int>> &vis, int** arr) {
+   int n = vis.size();
+   int m = vis[0].size();
+   vis[r][c] = -1;
+
+   for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
+         int nr = r+i;
+         int nc = c+j;
+         if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+            if (vis[nr][nc]==1) {
+               dfs(nr, nc, vis, arr);
+            } 
+         } 
+      }
+   }
+}
+int getTotalIslands(int** arr, int n, int m)
+{
+   // Write your code here.
+   vector<vector<int>> vis(n, vector<int> (m, 0));
+   for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+         vis[i][j] = arr[i][j];
+      }
+   }
+
+   int ans = 0;
+
+
+   for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+         if (vis[i][j] == 1) {
+            dfs(i, j, vis, arr);
+            ans++;
+         } 
+      }
+   }
+
+   return ans;
+}
+
+
+// USING BFS
 void bfs (int r, int c, vector<vector<int>> &vis, int** arr) {
    int n = vis.size();
    int m = vis[0].size();
