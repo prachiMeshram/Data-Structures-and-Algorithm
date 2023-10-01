7c90df2b67d3ace14de1dce815c983499c1eda44
Prachi Meshram <prachimesh89@gmail.com>
2023-10-01 12:40:05 +0530

    Dijkstra's shortest path using SET

--- SDE SHEET/Graph II/dijkstra'sShortestPath.cpp
+++ SDE SHEET/Graph II/dijkstra'sShortestPath.cpp
@@ -3,6 +3,49 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+// USING SET
+vector<int> dijkstra(vector<vector<int>> &vec, int vertices, int edges, int source) {
+    // Write your code here.
+    // USING SET
+    
+    set<pair<int, int>> st;
+    vector<int> dist(vertices, INT_MAX);
+
+    vector<pair<int, int>> adj[vertices];
+
+    for (auto it: vec) {
+        int src = it[0];
+        int des = it[1];
+        int wt = it[2];
+        adj[src].push_back({des, wt});
+        adj[des].push_back({src, wt});
+    }
+    dist[source] = 0;
+    st.insert({0, source});
+
+    while (!st.empty()) {
+        auto it = *st.begin();
+        int d = it.first;
+        int node = it.second;
+        st.erase(it);
+
+        for (auto it: adj[node]) {
+            int edgewt = it.second;
+            int adjNode = it.first;
+            
+            if(d + edgewt < dist[adjNode]) {
+                if (dist[adjNode] != 1e9) {
+                    st.erase({dist[adjNode], adjNode});
+                }
+                dist[adjNode] = d + edgewt;
+                st.insert({dist[adjNode], adjNode});
+            }
+        }
+    }
+    return dist;
+} 
+
+// USING PRIORITY QUQUE
 
 vector<int> dijkstra(vector<vector<int>> &vec, int vertices, int edges, int source) {
     // Write your code here.
